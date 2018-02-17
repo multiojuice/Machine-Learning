a3f41f7ac115f78de2dd6632b5ae7129251a17c5
Owen J Sullivan <multiojuice@gmail.com>
2018-02-16 20:42:03 -0500

    Initial Commit

---
+++ NeuralNet/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @author Owen Sullivan, Carter Nesbitt
+ * Setup for the first push
+ *
+ */
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
