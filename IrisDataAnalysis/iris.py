87af7dd068f7c531bcd7fa81a8435c0172cf049a
Owen J Sullivan <multiojuice@gmail.com>
2018-03-05 13:51:36 -0500

    Added functionalities and found best algorithms

--- IrisDataAnalysis/iris.py
+++ IrisDataAnalysis/iris.py
@@ -50,3 +50,61 @@ scatter_matrix(dataset)
 plt.show()
 
 
+# Get the data to train, then get the data to predict
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+validation_size = .20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+scoring = "accuracy"
+
+
+# Test several different algorithms to see
+# What will work best for our problem
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+
+# Evaluate every type of model we set up
+results = []
+names = []
+
+
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model,X_train,Y_train,cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
+    
+# Viewing these scores, we see that KNN
+# Has the best accuracy, so we will go with this :)
+
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
